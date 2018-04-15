9e30740c9300177773ffbf179b43ea9b318df6f9
robson.junior <robson.ga.junior@gmail.com>
2018-04-15 13:05:10 -0300

    [+] generic middleware to set request headers

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+// importing modules
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-// view engine setup
+// import route and middleware
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let genericMiddlware = require("./middlewares/generic-middleware");
+
+// start express app
+let app = express();
+
+
+///////////////////////
+// VIEW SETUP ENGINE //
+//////////////////////
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+////////////////////////
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +32,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+////////////
+// ROUTES //
+////////////
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+////////////////
+// middlewares //
+////////////////
+
+app.use(genericMiddlware);
+
+app.use(function(request, response, next) {
+    // catch 404 and forward to error handler
+    next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(error, request, response, next) {
+    // error handler
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // set locals, only providing error in development
+    response.locals.message = error.message;
+    response.locals.error = request.app.get('env') === 'development' ? error : {};
+
+    // render the error page
+    response.status(error.status || 500);
+    response.render('error'); // not executed case it is not error
 });
+
 
 module.exports = app;
