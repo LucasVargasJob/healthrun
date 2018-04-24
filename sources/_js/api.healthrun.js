c49a47de609ce4fd4ce6fb93e35e27c32ee22ba6
robson.junior <robson.ga.junior@gmail.com>
2018-04-24 18:44:45 -0300

    [+] get and printing my current position with GoogleMapsApiJS

--- sources/_js/api.healthrun.js
+++ sources/_js/api.healthrun.js
@@ -19,5 +19,6 @@ let healthrunAPI = {};
     healthrunAPI.description = 'A service to show you a new and fuuny way to have a health life.';
     healthrunAPI.url = 'http://localhost';
     healthrunAPI.port = '3000';
+    healthrunAPI.gKey = 'AIzaSyCs_c5DE8zqlkS1rfLIe1CNuElsC0cVYCo';
 })();
 
--- sources/_js/context.js
+++ sources/_js/context.js
@@ -10,5 +10,54 @@
 (function context() {
     'use strict';
 
-    console.log('[*] context started');
+
+    let PRIVATE = {};
+
+    PRIVATE.createMapMyposition = function() {
+        if(!navigator.onLine) {
+            console.warn('[!!] INTERNET IS NOT AVAILABLE');
+            return;
+        }
+
+        if(!navigator.geolocation) {
+            console.warn('[!!] YOUR BROWSER DOES NOT SUPPORT GEOLOCATION');
+            return;
+        }
+
+        let googleMapContent = document.getElementById('googleMapsContent');
+        let map = new google.maps.Map(googleMapContent, {
+            center: {lat: -34.397, lng: 150.644}, // Sydney
+            zoom: 6
+        });
+        let infoWindow = new google.maps.InfoWindow; // jshint ignore: line
+        let timeStart = new Date();
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log('[*] CURRENT POSITION: ', position);
+            console.log('[*] TIME TO GET POSITION: ', (new Date() - timeStart) / 1000); // calc how many time to get position
+
+            if(
+                !position.coords.latitude ||
+                !position.coords.longitude
+            ) {
+                return;
+            }
+
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function(error) {
+            console.log('[!!] WAS NOT POSSIBLE TO GET YOUR LOCATION');
+            console.error(error);
+        });
+    };
+
+
+    PRIVATE.createMapMyposition();
 })();
